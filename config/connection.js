52b4d49cd0a53ebafadf2c10d5dd218ac3c5ab62
anugrahsfutura <anugrah.futura@gmail.com>
2022-04-09 15:24:58 +0530

    mongoclient

--- config/connection.js
+++ config/connection.js
@@ -1,35 +1,9 @@
-// // const mongoClient=require('mongodb').mongoClient;
-// const mongoose=require("mongoose");
-// // mongoose.connect("mongodb://localhost:27017/LibraryApp");
-// mongoose.connect("mongodb+srv://libraryapp:libraryapp@clusterlibrary.5fslb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
-
-// const Schema=mongoose.Schema;
-
-// const BookSchema=new Schema({
-//     BookName:String,
-//     AuthorName:String,
-//     BookType:String,
-//     BookYear:Number,
-//     Image:String
-
-// });
-// let bookdata= mongoose.model('BookData',BookSchema);
-// module.exports=bookdata;
-// // const LogInSchema=new Schema({
-// //     Email:String,
-// //     Password:String
-// // })
-
-// // module.exports=LogIn;
+const mongoClient=require('mongodb').mongoClient;
+const url="mongodb://localhost:27017/LibraryApp"
 
 
-// // module.exports={
-// //     bookdata:function(){
-// //         let bookdata= mongoose.model('BookData',BookSchema);
+mongoClient.connect(url,(err)=>{
+    console.log(err);
+})
 
-// //     },
-// //     LogIn:function(){
-// //         let LogIn= mongoose.model('LogIn',LogInSchema);
-
-// //     }
-// // };
+module.exports=database;
